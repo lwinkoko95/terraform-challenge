5672b1c2eebc36c2bfe5f8e88b94e18b1407c941
lwinkoko95 <lwinkoko.cu@gmail.com>
2024-05-27 16:38:20 +0630

    terraform challenge 3

---
+++ terraform_challenge_3/variables.tf
@@ -0,0 +1,12 @@
+variable "ami"{
+    type = string
+    default = "ami-06178cf087598769c"
+}
+variable "instance_type" {
+    type = string
+    default = "m5.large"
+}
+variable "region" {
+    type = string
+    default = "eu-west-2"  
+}
